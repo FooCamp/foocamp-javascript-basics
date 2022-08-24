eec5cfe9463d73e6fecf8473f50ef803bf6c8e99
Federico Marroquin <fmarroquin@hugeinc.com>
2022-08-23 21:30:50 -0500

    init repository

---
+++ 5.exercises/exercises.js
@@ -0,0 +1,79 @@
+/**
+ * Exercise 1
+ * @param {number} a
+ * @param {string} b
+ * @return {Object}
+ */
+function dataTypes(a, b) {
+  //your code goes here...
+  
+  //1. Declare a variable and assign it the number 7.
+
+  //2. Declare a variable and assign it the string 'hello world'.
+  
+  //3. Create an object with 2 properties. name the first property as
+  // sum and the second property as concat.
+ 
+  //4. Assign the addition + between a and your first variable to the first object property.
+  
+  //5. Assign the concatenation between b and your second variable to the second object property. (Order is important, don't add spaces)
+  
+  //6. return object
+}
+
+/**
+ * Exercise 2
+ * @param {number} grossSalary
+ * @return {number}
+ */
+function operators(grossSalary) {
+  //your code goes here...
+
+  //1. Deduct the 4% for health contribution
+
+  //2. Deduct the 4% for pension.
+
+  //3. oh no, you are in a syndicate, you have to pay 20 to be there.
+
+  //4. The government decides you are an exemplary citizen and gives you 2. add it to your count.
+
+  //5. your salary is in dollars, multiply it for 4300 to pass it to colombian pesos.
+  
+  //6. Your wife just left you, so she takes the half of all of your money.
+
+  //7. return the value after applying the previous operations.
+}
+
+
+/**
+ * Exercise 3
+ * @param {number} n 
+ * @return {string}
+ */
+function conditionals(n) {
+  //your code goes here...
+
+  //1. if n is negative return the string 'negative'
+
+  //2. if n is in the inclusive range of 0 to 10 return the lowercase English word to each number (e.g., zero, one, two, etc);
+  
+  //3. if n is bigger than 10 return the string 'is way bigger'
+}
+
+/**
+ * Exercise 4
+ * @param {number} n 
+ * @return {number}
+ */
+function loops(n) {
+  //your code goes here...
+
+  //1. You can choose what loop statement to use. (e.g., for, while, for..of);
+
+  //2. calculate the factorial of n.
+
+  //3. return the value.
+
+}
+
+dataTypes();
